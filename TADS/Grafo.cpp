9e70ce789ee2e2d9e2e083aff5121b1ed636e7c4
TalkySafe143 <sebastian.galindo143@gmail.com>
2023-10-15 22:11:29 -0500

    Agregando el inicio del grafo

---
+++ TADS/Grafo.cpp
@@ -0,0 +1,5 @@
+//
+// Created by talkysafe143 on 15/10/23.
+//
+
+#include "Grafo.h"
---
+++ TADS/Grafo.h
@@ -0,0 +1,14 @@
+//
+// Created by talkysafe143 on 15/10/23.
+//
+
+#ifndef PROYECTO_GRAFO_H
+#define PROYECTO_GRAFO_H
+
+
+class Grafo {
+
+};
+
+
+#endif //PROYECTO_GRAFO_H
