4bfc1c796d959e8ea1fa147e3118f11ab8c03461
Sergio Barreto <ba.sergio@javeriana.edu.co>
2023-10-16 21:32:52 -0500

    cambios al main inicializar y turno(incompleto)

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include "TADS/Jugador.h"
 #include "TADS/ArbBin.h"
 #include "TADS/nodoB.h"
+#include "TADS/Interfaces.h"
 
 using namespace std;
 
@@ -23,13 +24,19 @@ int main() {
     cout << "Ingrese su comando: ";
     list<string> command;
     list<string>::iterator it;
+
+    // inicializar
+    Interfaces juego;
+    bool juegoIniciado = false;
+    bool juegoFinalizado = false;
+
     do { // Segmentation fault
         command.clear();
         char* str = new char[100];
         cin.getline(str, 50);
-
         cout << "Antes del switch\n";
         char *p = strtok(str, " ");
+
         if (p != nullptr) {
             while (p != nullptr){
                 command.emplace_back(p);
@@ -39,13 +46,51 @@ int main() {
             command.emplace_back(str);
         }
         it = command.begin();
+
+
         switch (evalString(*it)) {
             case inicializar: {
-                
+                if(!juegoIniciado){
+                    juego.inicializarJuego();
+                    juegoIniciado = true;
+                    cout << "(Inicialización satisfactoria) El juego se ha inicializado correctamente." << endl;
+                }else{
+                    cout << "(Juego en curso) El juego ya ha sido inicializado." << endl;
+                }
                 break;
             }
-            case turno: {
-                cout << "turno\n";
+            case turno: {                   //turno <id_jugador>
+                if (!juegoIniciado) {cout << "(Juego no inicializado) Esta partida no ha sido inicializada correctamente.\n"; break;}
+                if (juegoFinalizado) {cout << "(Juego terminado) Esta partida ya tuvo un ganador.\n";break;}
+
+                string playerIdInput = *it++;                            //id que me dan  <id_jugador>
+
+                //mirar si el jugador está en el juego
+                bool esta = false;
+                list<Jugador> jugadores = juego.getGame().getJugadores();
+                for(auto z : jugadores){
+                    if(z.getId() == playerIdInput) esta = true;
+                }
+                if(!esta) {cout<<"(Jugador no válido) El jugador <id_jugador> no forma parte de esta partida.\n"; break;}
+
+                //mirar si es el turno del jugador que me dan
+                string playerId = juego.getGame().getTurno()->getId();   //id del jugador que le toca
+                if (playerIdInput != playerId){ cout << "(Jugador fuera de turno) No es el turno del jugador <id_jugador>\n"; break;}
+
+
+                //hace lo del turno
+
+                // mira si ya gano (ej: conquistó todos los territorios)
+                if (playerHasWon()) {
+                    juegoFinalizado = true;
+                    cout << "(Juego terminado) El jugador " << playerId << " ha ganado." << endl;
+                } else {
+                    cout << "(Turno terminado correctamente) El turno del jugador " << playerId << " ha terminado."
+                         << endl;
+                }
+
+
+
                 break;
             }
             default:
